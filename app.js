c1b5b73397d9cf59dd045ae1eff15ce6a7c42053
Alina <almaksimovic01@gmail.com>
2022-10-26 17:43:55 +0300

    refactor: correction of formatting errors

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="ul__item__checkbox";
     editInput.type="text";
     editInput.className="task input-text";
 
@@ -88,7 +89,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-Mode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -102,7 +103,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-Mode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
